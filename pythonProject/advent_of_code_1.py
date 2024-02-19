95559b7f68095ffc01cc00454841281762981c9e
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-19 15:47:30 +0200

    Commiting fixed bad code structure of the first advent of code task

--- pythonProject/advent_of_code_1.py
+++ pythonProject/advent_of_code_1.py
@@ -1,21 +1,28 @@
-f = open("advent_of_code_1_input", 'r')
-txt = f.read()
-one_line = txt.splitlines()
+from pathlib import Path
+p = Path("advent_of_code_1_input")
+p = p.read_text()
+lines = p.splitlines()
 
-def get_number(string):
-    for i in string:
-        if i.isdigit():
-            first_value = i
+def get_number(line):
+
+    first_value = "0"
+    for char in line:
+        if char.isdigit():
+            first_value = char
             break
-    reversed_string = string[::-1]
-    for j in reversed_string:
-        if j.isdigit():
-            second_value = j
+
+    second_value = "0"
+    for char in reversed(line):
+        if char.isdigit():
+            second_value = char
             break
     return first_value+second_value
 
-sum_of_all_values = 0
-for element in one_line:
-    value = int(get_number(element))
-    sum_of_all_values += value
+def main():
+    sum_of_all_values = 0
+    for element in lines:
+        value = int(get_number(element))
+        sum_of_all_values += value
     print(sum_of_all_values)
+
+main()
