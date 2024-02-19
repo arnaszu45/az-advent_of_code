50c76e327549dd683d5a2af8310433589757725f
Arnas Žuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-19 14:57:17 +0200

    Commiting not completely done advent of code part 2

---
+++ pythonProject/advent_of_code_2.py
@@ -0,0 +1,25 @@
+d = {}
+d['one'] = '1'
+d['two'] = '2'
+d['three'] = '3'
+d['four'] = '4'
+d['five'] = '5'
+d['six'] = '6'
+d['seven'] = '7'
+d['eight'] = '8'
+d['nine'] = '9'
+
+f = open("advent_of_code_1_input", 'r')
+txt = f.read()
+one_line = txt.splitlines()
+
+a = "sixsrvldfour4seven"
+
+def converter(string, dictionary):
+    result = ''
+    i = 0
+    while i < len(string):
+        found = False
+        for word, number in dictionary.items():
+            if string.startswith(word, i):
+
