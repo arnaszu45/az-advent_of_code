ddcb0a844d2bfcc6b76469bcb369eb14babb2766
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-20 10:37:25 +0200

    Commititng second task ,the answer is correct, will be waiting for the review

--- pythonProject/advent_of_code_2.py
+++ pythonProject/advent_of_code_2.py
@@ -1,30 +1,25 @@
 from pathlib import Path
+
 p = Path("advent_of_code_1_input")
 p = p.read_text()
 lines = p.splitlines()
 
-dictionary = {}
-dictionary['one'] = '1'
-dictionary['two'] = '2'
-dictionary['three'] = '3'
-dictionary['four'] = '4'
-dictionary['five'] = '5'
-dictionary['six'] = '6'
-dictionary['seven'] = '7'
-dictionary['eight'] = '8'
-dictionary['nine'] = '9'
+word_number_map = {'one': '1', 'two': '2', 'three': '3', 'four': '4',
+                   'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
 
 def converter(line, dictionary):
     char = 0
     while char < len(line):
         for word, number in dictionary.items():
             if line.startswith(word, char):
-                line = line.replace(word, number)
+                replaced = (word.replace(word[1], number))
+                line = line.replace(word, replaced)
         char += 1
     return line
 
-def get_number(line):
 
+def get_number(line):
     first_value = "0"
     for char in line:
         if char.isdigit():
@@ -36,14 +31,16 @@ def get_number(line):
         if char.isdigit():
             second_value = char
             break
-    return first_value+second_value
+    return first_value + second_value
+
 
 def main():
     sum_of_all_values = 0
-    for element in lines:
-        converted_line = converter(element, dictionary)
+    for line in lines:
+        converted_line = converter(line, word_number_map)
         value = int(get_number(converted_line))
         sum_of_all_values += value
     print(sum_of_all_values)
+
 
 main()
