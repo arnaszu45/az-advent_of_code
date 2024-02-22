468b7586b3731e8325c907ccfbf8272037823bc9
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-22 09:22:18 +0200

    wip: implementing argparse in training task

--- pythonProject/training_task.py
+++ pythonProject/training_task.py
@@ -1,12 +1,21 @@
 from pathlib import Path
+import argparse
+search_for = {
+    'a': 'cleaning.start_cleaning(',
+    'b': 'shunts'
+}
+parser = argparse.ArgumentParser(description='Searching for function by name')
+parser.add_argument("function", type=str)
+args = parser.parse_args()
+function = search_for.get(args.function, "")
 
 
 def main():
     polarion_list = []
     for path in Path('TestAutomation').rglob("test_*.py"):
         file = path.read_text()
-        if 'cleaning.start_cleaning(' in file:
-            lines = file[file.find('ID: ') + 3:]
+        if function in file:
+            lines = file[file.find('Polarion ID: ') + 13:]
             lines = lines.split('\n')
             polarion_id = (lines[0])
             polarion_id = polarion_id.strip().split('\n')
