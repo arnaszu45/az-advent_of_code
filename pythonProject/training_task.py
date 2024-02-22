f078aa16de338c2522b646ceccc58206b8990b11
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-22 14:25:54 +0200

    wip: updating training task

--- pythonProject/training_task.py
+++ pythonProject/training_task.py
@@ -9,16 +9,17 @@ def main():
         if args.pattern in file:
             id_pattern = '"""\n    Polarion ID:'
             remaining_text = file[file.find(id_pattern) + len(id_pattern):]
-            remaining_text = remaining_text.split('\n', 1)[0]
+            remaining_text = remaining_text.split()[0]
             polarion_id = remaining_text.strip()
-            polarion_list.append(polarion_id)
+            if '-' in polarion_id:
+                polarion_list.append(polarion_id)
     print(polarion_list)
     print(len(polarion_list))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--test_automation_dir", type=Path, help="Directory of test automation folder")
+    parser.add_argument("-d", "--test_automation_dir", type=Path, nargs="?", help="Directory of test automation folder")
     parser.add_argument("-p", "--pattern", type=str, help="The pattern what should be looked for in files")
     args = parser.parse_args()
     main()
