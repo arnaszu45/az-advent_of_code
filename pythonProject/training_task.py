688f093b4fb0e52f3bc9446957603199a7300e13
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-23 09:25:21 +0200

    wip: updated training_task and finished aoc day 2 part 2

--- pythonProject/training_task.py
+++ pythonProject/training_task.py
@@ -2,24 +2,27 @@ from pathlib import Path
 import argparse
 
 
-def main():
+def main(directory: Path, pattern: str):
     polarion_list = []
-    for path in args.test_automation_dir.rglob("test_*.py"):
-        file = path.read_text()
-        if args.pattern in file:
-            id_pattern = '"""\n    Polarion ID:'
-            remaining_text = file[file.find(id_pattern) + len(id_pattern):]
+    for path in directory.rglob("test_*.py"):
+        file_text = path.read_text()
+        if pattern in file_text:
+            id_pattern = 'Polarion ID:'
+            id_index = file_text.find(id_pattern)
+            if id_index < 0:
+                continue
+            remaining_text = file_text[id_index + len(id_pattern):]
             remaining_text = remaining_text.split()[0]
             polarion_id = remaining_text.strip()
-            if '-' in polarion_id:
-                polarion_list.append(polarion_id)
+            polarion_list.append(polarion_id)
     print(polarion_list)
     print(len(polarion_list))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--test_automation_dir", type=Path, nargs="?", help="Directory of test automation folder")
+    parser.add_argument("-d", "--test_automation_dir", type=Path, help="Directory of test automation folder")
     parser.add_argument("-p", "--pattern", type=str, help="The pattern what should be looked for in files")
+    # parser.add_argument(, "--pattern", nargs='+', type=str, help="The pattern what should be looked for in files")
     args = parser.parse_args()
-    main()
+    main(directory=args.test_automation_dir, pattern=args.pattern)
