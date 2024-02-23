f1e53c1123a6e1694238dac0337ba822dc3893a5
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-02-23 15:04:25 +0200

    wip: updating training_task

--- pythonProject/training_task.py
+++ pythonProject/training_task.py
@@ -4,24 +4,35 @@ import argparse
 
 def main(directory: Path, pattern: str):
     polarion_list = []
-    for path in directory.rglob("test_*.py"):
-        for i, line in enumerate(reversed(list(open(path)))):
-            reversed_file = line.rstrip()
-            if pattern in reversed_file:
-                print(pattern)
-                # print(reversed_file[::pattern])
-        # file_text = path.read_text()
+    single_files = [path for path in Path(directory).rglob("*.py")]     # All python's files
 
-
-#     if pattern in file_text:
-    #         id_pattern = 'Polarion ID:'
-    #         id_index = file_text.find(id_pattern)
-    #         if id_index < 0:
-    #             continue
-    #         remaining_text = file_text[id_index + len(id_pattern):]
-    #         remaining_text = remaining_text.split()[0]
-    #         polarion_id = remaining_text.strip()
-    #         polarion_list.append(polarion_id)
+    for path in single_files:   # Collecting single file's path
+        file_text = path.read_text(encoding="UTF-8")    # Making all files readable
+        if pattern in file_text:
+            id_pattern = 'Polarion ID:'
+            if id_pattern in file_text:
+                id_index = file_text.find(id_pattern)
+                if id_index < 0:
+                    continue
+                remaining_text = file_text[id_index + len(id_pattern):]
+                remaining_text = remaining_text.split()[0]
+                polarion_id = remaining_text.strip()
+                polarion_list.append(polarion_id)
+            else:
+                print(file_text.index(pattern))
+                res = file_text[:file_text.index(pattern) + len(pattern)]
+                indirect_function = res[file_text.index(pattern) - 300:]
+                indirect_function =
+                print(search_function_name)
+                # breakpoint()
+    # print(polarion_list)
+    print(len(polarion_list))
+            # if id_index < 0:
+            #     continue
+            # remaining_text = file_text[id_index + len(id_pattern):]
+            # remaining_text = remaining_text.split()[0]
+            # polarion_id = remaining_text.strip()
+            # polarion_list.append(polarion_id)
     # print(polarion_list)
     # print(len(polarion_list))
 
