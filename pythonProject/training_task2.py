a7214cdb7928a1d8c85db366a46c33611ce8e0b9
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-03-14 09:46:49 +0200

    wip: updates second training task

--- pythonProject/training_task2.py
+++ pythonProject/training_task2.py
@@ -19,7 +19,7 @@ def search_for_setup_name(file_text: str, pattern: str) -> str:
         return ''
 
 
-def make_dictionary_of_setup_name_and_protocol(filename: str, directory: str) -> dict:
+def make_dictionary_of_setup_name_and_protocol(filename: str, directory: Path) -> dict:
     """Collect protocols from XML according to setup name and keeps it in dictionary"""
 
     dict_of_elements_and_test_type = {}
@@ -28,11 +28,14 @@ def make_dictionary_of_setup_name_and_protocol(filename: str, directory: str) ->
     protocols = root.findall('.//protocol')
     for element in protocols:
         script = element.find('.//test-script-reference').text
-        path = script[script.find('test_cases/'): script.find('.py') + 3]   ## Also modify here
-        file_name = Path(directory + '/' + path)                            ## Fix this part 4AP2-38092
+        test_case_file_name = script[script.find('test_cases/') + 11: script.find('.py') + 3]
+
+        file_name = os.path.join(directory, test_case_file_name)                        ## Fix this part 4AP2-38092
+        full_path = Path(file_name)
+
         test_type = 'unknown'
-        if file_name.exists():
-            file_text = file_name.read_text(encoding="UTF-8")
+        if full_path.exists():
+            file_text = full_path.read_text(encoding="UTF-8")
             if 'Title: Semi-automated:' in file_text:
                 test_type = 'semi-automated'
             elif 'Setup: ' in file_text:
@@ -43,10 +46,12 @@ def make_dictionary_of_setup_name_and_protocol(filename: str, directory: str) ->
         test_case_list.append(element)
         dict_of_elements_and_test_type[test_type] = test_case_list
 
+    if len(dict_of_elements_and_test_type) == 1:
+        raise Exception("-- Probably was given wrong 'test_cases' directory -- ")
     return dict_of_elements_and_test_type
 
 
-def distribute_protocols_through_files(filename: str, dictionary: dict):
+def distribute_protocols_through_files(filename: str, dictionary: dict, new_folder: str):
     """Distributes protocols from given XML file into separated new XML files by setup names"""
 
     os.makedirs('output', exist_ok=True)
@@ -62,29 +67,30 @@ def distribute_protocols_through_files(filename: str, dictionary: dict):
         new_protocols.extend(values)
 
         new_tree = Et.ElementTree(new_root)
-        setup_filename = os.path.join('output', f"{key}.xml")
+        setup_filename = os.path.join(new_folder, f"{key}.xml")
         new_tree.write(setup_filename, encoding='utf-8', xml_declaration=True)
 
 
-def main(filename: str, directory: str):            # Need some prints about running the script
+def main(filename: str, directory: Path, new_folder: str):
+    print('--- Running the script ---') # <-- Are prints in here legal??
     dictionary = make_dictionary_of_setup_name_and_protocol(filename, directory)
-    distribute_protocols_through_files(filename, dictionary)
+    distribute_protocols_through_files(filename, dictionary, new_folder)
+    amount_of_files_in_directory = len(os.listdir(new_folder))
+    print(f'Was generated {amount_of_files_in_directory} new files from {filename}')
 
 
 if __name__ == '__main__':
     start = time.time()
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("-f", "--xml_file", required=True, type=Path,
-    #                     help="File name or path of xml file which has to be proceeded")
-    # parser.add_argument("-d", "--test_automation_dir", required=True, type=Path,  # Not correct will look for another approach
-    #                     help="Directory of test automation folder")
-    #
-    # args = parser.parse_args()
-    # main(filename=args.xml_file, directory=args.test_automation_dir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--xml_file", required=True, type=Path,
+                        help="File name or path of xml file which has to be proceeded")
+    parser.add_argument("-d", "--test_automation_dir", required=True, type=Path,
+                        help="Directory of test automation folder + test_cases directory, - <"
+                             "test_automation/test_cases/>")
+    parser.add_argument("-n", "--new_folder", required=True, type=str,
+                        help="A name of new folder, where xml files will be saved after writing") # Don't know how to write it nicely
+    args = parser.parse_args()
 
-    export_file = "export.xml"              # Add argparse
-    test_automation_dir = 'TestAutomation'   # Add argparse
-    main(export_file, test_automation_dir)
-
+    main(filename=args.xml_file, directory=args.test_automation_dir, new_folder=args.new_folder)
     end_time = time.time()
     print(end_time - start)
