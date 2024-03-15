0be87af2ed13e9178345be543a4c7ea468db7b8f
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-03-15 10:19:25 +0200

    wip: sort_protocols updated, implemented parse_xml_file function

--- pythonProject/sort_protocols.py
+++ pythonProject/sort_protocols.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 import os
 import time
 import argparse
-import traceback
+import sys
 
 
 def search_for_setup_name(file_text: str, pattern: str) -> str:
@@ -20,18 +20,15 @@ def search_for_setup_name(file_text: str, pattern: str) -> str:
         return ''
 
 
-def make_dictionary_of_setup_name_and_protocol(filename: Path, directory: Path) -> dict:
-    """Collect protocols from XML according to setup name and keeps it in dictionary"""
+def categorise_protocols_by_setup(directory: Path, protocols: list[Et.Element]) -> dict:
+    """Collects protocols from XML file, categorise by setup name and keeps it in dictionary"""
 
     dict_of_elements_and_test_type = {}
-    tree = Et.parse(filename)
-    root = tree.getroot()
-    protocols = root.findall('.//protocol')
     for element in protocols:
         test_type = 'unknown'
         script = element.find('test-script-reference').text
         if script[script.find('/test_cases/'): script.find('.py') + 3]:
-            test_case_file_name = script[script.find('/test_cases/'): script.find('.py') + 3]  ## Handle random name
+            test_case_file_name = script[script.find('/test_cases/'): script.find('.py') + 3]
             file_name = f'{directory}{test_case_file_name}'  # 4AP2-38092
             full_path = Path(file_name)
 
@@ -42,35 +39,29 @@ def make_dictionary_of_setup_name_and_protocol(filename: Path, directory: Path) 
                 elif 'Setup: ' in file_text:
                     setup_name = search_for_setup_name(file_text, 'Setup: ')
                     test_type = f'{setup_name.lower()}'
-        else:
-            print(f"{Et.tostring(element)}\n !!! WARNING, Protocol path is damaged, adding to 'unknown.xml' !!!\n")
+        else: # Fix here, do everything above
+            print(f"{script}\n !!! WARNING, Protocol path is damaged, adding to 'unknown.xml' !!!\n") # do if not above instead of going down
         test_case_list = dict_of_elements_and_test_type.get(test_type, [])  # Get is able to return empty list
         test_case_list.append(element)
         dict_of_elements_and_test_type[test_type] = test_case_list
 
     if len(dict_of_elements_and_test_type) == 1:
-        try:
-            raise TypeError
-        except TypeError:
-            print("-- Probably was given wrong 'test_automation' directory -- ")
-            traceback.print_exception(quit(), limit=0)
+        print("-- Probably was given wrong 'test_automation' directory -- ")
+        sys.exit()
 
     return dict_of_elements_and_test_type
 
 
-def distribute_protocols_through_files(filename: Path, dictionary: dict, new_folder: str):
+def distribute_protocols_through_files(setup_name_and_protocol: dict, new_folder: str, root: Et.Element):
     """Distributes protocols from given XML file into separated new XML files by setup names"""
 
     try:
         os.makedirs(new_folder)
     except FileExistsError:
         print(f"!!! ERROR: Folder {new_folder} already exists. Use another folder name or delete existing one !!!")
-        traceback.print_exception(quit(), limit=0)
+        sys.exit()
 
-    tree = Et.parse(filename)
-    root = tree.getroot()
-
-    for key, values in dictionary.items():
+    for key, values in setup_name_and_protocol.items():
         new_root = deepcopy(root)
         new_protocols = new_root.find('.//protocols')
         new_protocols.clear()
@@ -82,12 +73,25 @@ def distribute_protocols_through_files(filename: Path, dictionary: dict, new_fol
         new_tree.write(setup_filename, encoding='utf-8', xml_declaration=True)
 
 
-def main(filename: Path, directory: Path, new_folder: str):
-    print('--- Running the script ---\n')  # <-- Are prints in here legal?? I mean in main
-    dictionary = make_dictionary_of_setup_name_and_protocol(filename, directory)
-    distribute_protocols_through_files(filename, dictionary, new_folder)
-    amount_of_files_in_directory = len(os.listdir(new_folder))
-    print(f'Was generated {amount_of_files_in_directory} new files from {filename}')
+def parse_xml_file(xml_file_name: Path):
+    """"Parses given XML file and returns root and specified SubElement """
+
+    try:
+        tree = Et.parse(xml_file_name)
+        root = tree.getroot()
+        protocols = root.findall('.//protocol')
+        return root, protocols
+    except Et.ParseError as e:
+        print(f'!!! ERROR, can not parse this XML file, error message: !!!\n{e}'), sys.exit()
+
+
+def main(export_file_name: Path, test_automation_dir: Path, output_folder: str):
+    print('--- Running the script ---\n')  # <-- Make logs
+    root, protocols = parse_xml_file(export_file_name)
+    protocols_by_setup = categorise_protocols_by_setup(test_automation_dir, protocols)
+    distribute_protocols_through_files(protocols_by_setup, output_folder, root)
+    amount_of_files_in_directory = len(os.listdir(output_folder))
+    print(f'Was generated {amount_of_files_in_directory} new files from {export_file_name}')
 
 
 if __name__ == '__main__':
@@ -97,9 +101,9 @@ if __name__ == '__main__':
                         help="File name or path of xml file which has to be proceeded")
     parser.add_argument("-d", "--test_automation_dir", required=True, type=Path,
                         help="Directory of test automation folder")
-    parser.add_argument("-n", "--new_folder", required=True, type=str,
-                        help="A name of new folder, where xml files will be saved after writing")  # Don't know how to write it nicely
+    parser.add_argument("-n", "--new_folder", required=True, type=Path,
+                        help="A name of new folder, where xml files will be saved after writing")
     args = parser.parse_args()
-    main(filename=args.xml_file, directory=args.test_automation_dir, new_folder=args.new_folder)
+    main(export_file_name=args.xml_file, test_automation_dir=args.test_automation_dir, output_folder=args.new_folder)
     end_time = time.time()
     print(end_time - start)
