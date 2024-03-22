9be85b527a1e6f8774516a95137125365901520e
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-03-22 14:56:41 +0200

    wip: updating collect_git_info

--- pythonProject/collect_git_info.py
+++ pythonProject/collect_git_info.py
@@ -1,17 +1,11 @@
-# What has to be done:
-# Take git repository
-# check if git repos
-# Print all commits in given directory
-# Collect commits into the list
-# for loop through all the commits
-# collect all the info from commit
-# put that into json file <- commits info
 import logging
 import argparse
 import subprocess
 from pathlib import Path
 import os
 import sys
+import re
+import json
 
 
 def check_git_repository(git_repo: Path):
@@ -28,7 +22,70 @@ def check_git_repository(git_repo: Path):
             sys.exit(1)
 
 
-def parse_args():
+def get_commits(git_repo: Path) -> list[str]:
+    """Collects all commit names from given Git repository and put them into list"""  # Correct description??
+
+    commits = []
+    git_log_output = subprocess.run(["git", "log"], shell=True, capture_output=True, text=True,
+                                    cwd=git_repo).stdout
+    lines = git_log_output.splitlines()
+    for line in lines:
+        if line.startswith("commit"):
+            single_commit = line.split()[1]
+            commits.append(single_commit)
+    return commits
+
+
+def get_author_from_git_log(commit_log: str) -> str:
+    """Finds and returns 'Author' from Git log message"""
+
+    match = re.search(r"Author: (.+)", commit_log)
+    if match is None:
+        logger.warning("Could not find Author in commit log")
+        return ""
+    else:
+        return match.group(1)
+
+
+def get_date_from_git_log(commit_log: str) -> str:
+    """Finds and returns 'Date' from Git log message"""
+
+    match = re.search(r"Date: (.+)", commit_log)
+    if match is None:
+        logger.warning("Could not find Date in commit log")
+        return ""
+    else:
+        return match.group(1)
+
+
+def get_message_from_git_log(commit_name: str) -> str:  # Different approach than other two
+    message = subprocess.run(["git", "show", "-s", "--format=%B", commit_name], shell=True, capture_output=True,
+                             text=True, encoding="UTF-8").stdout
+    return message
+
+
+def get_file_names_from_git_log(commit_name: str) -> str:
+    file_name = subprocess.run(["git", "show", "--pretty=""", "--name-only", commit_name],
+                               shell=True, capture_output=True, text=True, encoding="UTF-8").stdout
+    return file_name
+
+
+def get_commit_info(git_repo: Path):
+    list_of_commits = get_commits(git_repo)
+    for single_commit in list_of_commits:
+        full_info_of_commit = subprocess.run(["git", "show", single_commit, "--stat"], shell=True, capture_output=True,
+                                            encoding="UTF-8").stdout
+        author = get_author_from_git_log(full_info_of_commit).strip()
+        date = get_date_from_git_log(full_info_of_commit).strip()
+        message = get_message_from_git_log(single_commit).strip()
+        changed_files = get_file_names_from_git_log(single_commit)
+
+        print(full_info_of_commit)
+        print(f"{single_commit}\n{author}\n{date}\n{message}\n{changed_files}\n")
+
+        break
+
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", "--git-repository", type=Path, required=True,
                         help="Specify the path to the git repository directory")
@@ -53,6 +110,7 @@ def configure_logger(filename: str) -> logging.Logger:
 def main(logger: logging.Logger):
     logger.info(" >>> Running the script\n")
     check_git_repository(args.git_repository)
+    get_commit_info(args.git_repository)
     logger.info(" >>> Done")
 
 
