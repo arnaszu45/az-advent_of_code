563bed53110efd62396c0da364e70657974627da
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-03-25 12:28:11 +0200

    wip: added function to create json file

--- pythonProject/collect_git_info.py
+++ pythonProject/collect_git_info.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 import sys
 import re
+import json
 
 
 def is_git_repo(git_repo: Path):
@@ -15,10 +16,11 @@ def is_git_repo(git_repo: Path):
     result = subprocess.run(["git", "-C", str(git_repo), "rev-parse"])
     if result.returncode != 0:
         logger.error(f"Error: {git_repo} is not a git repository.")
+
     return "Given repository is valid"
 
 
-def get_commits(git_repo: Path) -> list[str]:  # here text = true must, otherwise it returns bytes
+def get_commits(git_repo: Path) -> list[str]:
     """Collects all commit hashes from given Git repository and put them into list"""
 
     commits = []
@@ -42,7 +44,7 @@ def get_author_from_git_log(commit_log: str) -> str:
     match = re.search(r"Author: (.+)", commit_log)
     if match is None:
         logger.warning("Could not find Author in commit log")
-        return ""
+        return "No author"
     return match.group(1).strip()
 
 
@@ -52,7 +54,7 @@ def get_date_from_git_log(commit_log: str) -> str:
     match = re.search(r"Date: (.+)", commit_log)
     if match is None:
         logger.warning("Could not find Date in commit log")
-        return ""
+        return "No date"
     return match.group(1).strip()
 
 
@@ -62,34 +64,66 @@ def get_message_from_git_log(commit_hash: str) -> str:
     return message.strip()
 
 
-def get_file_names_from_git_log(commit_hash: str) -> str:
-    file_name = subprocess.run(["git", "show", "--pretty=""", "--name-only", commit_hash],
-                               shell=True, capture_output=True, text=True, encoding="UTF-8").stdout
-    return file_name
+def get_file_names_from_git_log(commit_hash: str) -> list:
+    file_names_output = subprocess.run(["git", "show", "--pretty=""", "--name-only", commit_hash],
+                                       shell=True, capture_output=True, text=True, encoding="UTF-8").stdout
+    file_names_list = file_names_output.strip().split('\n')
+
+    return file_names_list
 
 
-def get_commit_info(git_repo: Path):
+def get_insertions_from_git_log(commit_log: str) -> str:
+    pattern = r"\d+.insertions\(\+\)"
+    match = re.search(pattern, commit_log)
+    if match is None:
+        logger.warning("Could not find any insertions in commit log")
+        return "0 insertions(+)"
+    return match.group()
+
+
+def get_deletions_from_git_log(commit_log: str) -> str:
+    pattern = r"\d+.deletions\(\-\)"
+    match = re.search(pattern, commit_log)
+    if match is None:
+        logger.warning("Could not find any deletions in commit log")
+        return "0 deletions(-)"
+    return match.group()
+
+
+def get_commit_info(git_repo: Path) -> list:
+    all_commits_data = []
     commits = get_commits(git_repo)
-    for single_commit in commits:
-        full_info_of_commit = subprocess.run(["git", "show", single_commit, "--stat"], shell=True, capture_output=True,
-                                            encoding="UTF-8").stdout
-        author = get_author_from_git_log(full_info_of_commit)
-        date = get_date_from_git_log(full_info_of_commit)
-        message = get_message_from_git_log(single_commit)
-        changed_files = get_file_names_from_git_log(single_commit)
+    for commit_hash in commits:
+        full_info_of_commit = subprocess.run(["git", "show", commit_hash, "--stat"], shell=True,
+                                             capture_output=True, encoding="UTF-8").stdout
+        commit_data = {
+            f"Commit hash: {commit_hash}": {
+                "Author: ": get_author_from_git_log(full_info_of_commit),
+                "Date: ": get_date_from_git_log(full_info_of_commit),
+                "Message: ": get_message_from_git_log(commit_hash),
+                "Changed_files: ": get_file_names_from_git_log(commit_hash),
+                "Insertions: ": get_insertions_from_git_log(full_info_of_commit),
+                "Deletions: ": get_deletions_from_git_log(full_info_of_commit)
+            }
+        }
+        all_commits_data.append(commit_data)
 
-        print(full_info_of_commit)
-        print(f"{single_commit}\n{author}\n{date}\n{message}\n{changed_files}\n")
+    return all_commits_data
 
-        break
+
+def create_json_file(data, json_file_name):
+    with open(json_file_name, "w") as file:
+        json.dump(data, file, indent=4)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", "--git-repository", type=Path, required=True,
                         help="Specify the path to the git repository directory")
+    parser.add_argument("-j", "--json_file", type=Path, default="git_info.json",
+                        help="Specify the json file name, where json output will be kept")
     parser.add_argument("-l", "--log-file", default='git_info.log',
-                        help="Specify the file name where logs should be kept")
+                        help="Specify the file name, where logs should be kept")
     return parser.parse_args()
 
 
@@ -110,8 +144,9 @@ def main(logger: logging.Logger):
     logger.info(" >>> Running the script\n")
     git_repository = is_git_repo(args.git_repository)
     if git_repository != "Given repository is valid":
-        sys.exit(1)
-    get_commit_info(args.git_repository)
+        sys.exit(1)  # Should here be logger or reman inside the function?
+    commits_data = get_commit_info(args.git_repository)
+    create_json_file(commits_data, args.json_file)
     logger.info(" >>> Done")
 
 
--- pythonProject/experiments_with_functions.py
+++ pythonProject/experiments_with_functions.py
@@ -1,54 +1,74 @@
-import os
-import sys
-import xml.etree.ElementTree as Et
+import re
+import subprocess
+import json
+
+string = """
+commit ddcb0a844d2bfcc6b76469bcb369eb14babb2766
+Author: arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
+Date:   Tue Feb 20 10:37:25 2024 +0200
+
+    Commititng second task ,the answer is correct, will be waiting for the review
+
+ pythonProject/advent_of_code_2.py | 29 +++++++++++++----------------
+ 1 file changed, 13 insertions(+), 16 deletions(-)
+"""
 
 
-def find_test_case_name(string: str) -> str:
-    start_position = string.find("/test_cases/")
-    if start_position == -1:
-        print(f'!!! ERROR, given protocol is damaged:\n {string}\n')
-        return ''
-    else:
-        remaining_script = string[start_position:]
-        root, ext = os.path.splitext(remaining_script)
-        return root + ext
+def get_author_from_git_log(commit_log: str) -> str:
+    """Finds and returns 'Author' from Git log message"""
+
+    match = re.search(r"Author: (.+)", commit_log)
+    if match is None:
+        return ""
+    return match.group(1).strip()
 
 
-def get_full_name_from_http(element: Et.Element, root: Et.Element):
-    sub_element = 'test-script-reference'
-    script_reference = element.find(sub_element)
-    if script_reference is None:
-        print(f'!!! ERROR, given {sub_element} sub element does not exists in given XML file')
-        sys.exit(1) # Should it better return '' or exiting is better ??
-    if script_reference.text is None:
-        print(f'!!! ERROR, given {sub_element} sub element does not exists in given XML file')
-        sys.exit(1) # Should it better return '' or exiting is better ??
-    if 'href' in script_reference.text:
-        # href = element.attrib['href']
-        # href = script_reference.attrib['href']
-        href = script_reference.find('a') #None
-        # href = element.find('a')
-        # href = element.findall('a')
-        # href = element.get('href') #None
-        # href = element.find('a') # None
-        # link = href.get('href')
-        # href = script_reference.find('a')
-        # link = href.get('href') # NoneType' object has no attribute 'get'
-        # return href
-        return href
-    return script_reference.text
+def get_date_from_git_log(commit_log: str) -> str:
+    """Finds and returns 'Date' from Git log message"""
+
+    match = re.search(r"Date: (.+)", commit_log)
+    if match is None:
+        return ""
+    return match.group(1).strip()
 
 
-# find attribute ['href']
+def get_message_from_git_log(commit_hash: str) -> str:
+    message = subprocess.run(["git", "show", "-s", "--format=%B", commit_hash], shell=True, capture_output=True,
+                             text=True, encoding="UTF-8").stdout
+    return message.strip()
 
 
-def main():
-    xml_file_name = 'test_output.xml'
-    tree = Et.parse(xml_file_name)
-    root = tree.getroot()
-    for element in root.findall(".//protocol"):
-        file_name_from_http = get_full_name_from_http(element, root)
-        print(file_name_from_http)
+def get_file_names_from_git_log(commit_hash: str) -> str:
+    file_name = subprocess.run(["git", "show", "--pretty=""", "--name-only", commit_hash],
+                               shell=True, capture_output=True, text=True, encoding="UTF-8").stdout
+    return file_name.strip()
 
 
-main()
+def get_insertions_from_git_log(commit_log: str) -> str:
+    pattern = r"\d+.insertions\(\+\)"
+    match = re.search(pattern, commit_log)
+    if match is None:
+        return "0 insertions(+)"
+    return match.group()
+
+
+def get_deletions_from_git_log(commit_log: str) -> str:
+    pattern = r"\d+.deletions\(\-\)"
+    match = re.search(pattern, commit_log)
+    if match is None:
+        return "0 deletions(-)"
+    return match.group()
+
+
+commit_hash = "Ecdwsxsd4444"
+data = {
+        f"Commit_hash : {commit_hash}": {
+            "Author: ": get_author_from_git_log(string),
+            "Date: ": get_date_from_git_log(string),
+            "Insertions: ": get_insertions_from_git_log(string),
+            "Deletions: ": get_deletions_from_git_log(string)
+        }
+}
+
+with open("git_info_exp.json", "w") as f:
+    json.dump(data, f, indent=4)
