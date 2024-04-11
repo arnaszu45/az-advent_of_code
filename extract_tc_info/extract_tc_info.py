41b3463c7e0f3fe6fb0c6d86e8e281c41d66e753
arnas.zuklija <arnas.zuklija@qdevtechnologies.com>
2024-04-11 09:29:41 +0300

    wip: updating extract test cases info

--- extract_tc_info/extract_tc_info.py
+++ extract_tc_info/extract_tc_info.py
@@ -1,8 +1,13 @@
 import argparse
+import json
 import logging
-from pathlib import Path
+import os.path
 import sys
 import time
+from datetime import datetime
+from pathlib import Path
+
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +15,11 @@ logger = logging.getLogger(__name__)
 def collect_test_cases_from_directory(test_automation_directory: Path) -> list[Path]:
     """From given TA directory, collects all 'test_*.py' files from '/test_cases/'"""
 
-    test_cases_directory = Path(f"{test_automation_directory}/test_cases")
+    if not isinstance(test_automation_directory, Path):
+        logger.error(f"{test_automation_directory} - invalid path")
+        return []
+
+    test_cases_directory = test_automation_directory / "test_cases"
 
     if not test_cases_directory.is_dir():
         logger.error(f"{test_cases_directory} - is not a valid directory path")
@@ -23,6 +32,72 @@ def collect_test_cases_from_directory(test_automation_directory: Path) -> list[P
         return []
 
     return test_cases_files
+
+
+def fetch_test_runs_data() -> dict:
+    """Fetches test runs data from a remote server, returning the response JSON data as a formatted string."""
+
+    url = "http://10.208.1.21:12001/test_runs"
+    params = {
+        "project": "Test Automation",
+        "limit": 50000000
+    }
+    headers = {
+        "accept": "application/json"
+    }
+
+    response = requests.get(url, params=params, headers=headers, data=json.dumps(params),
+                            timeout=5)  # Need timeout exception
+    if response.status_code != 200:
+        logger.error(
+            f"Failed to fetch data. URL: {response.url}\nStatus: {response.status_code}\nReason: {response.reason}\n")
+        return {}
+
+    return response.json()
+
+
+def get_latest_submission_time(run_test_data: dict, test_case_file_name: str) -> str:
+    """From Run tests data extract the latest execution date of test case"""
+
+    submission_times = []
+    if "builds" not in run_test_data:
+        logger.error("Key 'builds' does not exists in given data")
+        return ""
+
+    for build in run_test_data["builds"]:
+        try:
+            tests = build["properties"]["test_scenario"]["tests"]
+        except KeyError as e:
+            logger.error(f"Missing or incorrectly formatted data in build properties - {e}.")
+            return ""
+
+        for test in tests:
+            if test.get("test_file") != test_case_file_name:
+                continue
+
+            submission_time = test.get("submission_time")
+            datetime_object = datetime.strptime(submission_time, "%Y_%m_%d_%Hh_%Mm")
+            formatted_time = datetime_object.strftime("%Y-%m-%d %H:%M")
+            submission_times.append(formatted_time)
+
+    if not submission_times:
+        return "Tests case was never executed"
+
+    return max(submission_times)
+
+
+def write_test_cases_data(test_automation_directory: Path) -> dict:
+    test_cases = collect_test_cases_from_directory(test_automation_directory)
+    test_runs_data = fetch_test_runs_data()
+    print(test_runs_data)
+    for test_case in test_cases:
+        test_case_file_name = os.path.basename(test_case)
+        last_execution_date = get_latest_submission_time(test_runs_data, test_case_file_name)
+        print(test_case_file_name)
+        test_case_data = {
+            "test_case_file_name": test_case_file_name,
+            "latest_submission_date": last_execution_date,
+        }
 
 
 def parse_args() -> argparse.Namespace:
@@ -53,9 +128,8 @@ def configure_logger(filename: str) -> logging.Logger:
 
 def main(logger_: logging.Logger):
     logger_.info(" >>> Running the script\n")
-    test_cases_files = collect_test_cases_from_directory(args.test_automation_directory)
-    if not test_cases_files:
-        sys.exit(1)
+
+    test_cases_data = write_test_cases_data(args.test_automation_directory)
 
 
 if __name__ == "__main__":
